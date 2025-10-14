1e37ed4dd0910b2344c40a7870eb8812e463fd2b
pedropompeu <pedrolpompeu@gmail.com>
2025-10-14 19:14:54 -0300

    feat(auth): implement jwt strategy and protect tasks routes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, UseGuards, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
-export class TasksController {}
+
+export class TasksController {
+    @Get()
+    getTasksTest(): string {
+        return 'Rota de tarefa protegida';
+    }
+}
