3701c8632685afc6a9ada260735c18b7659d20bb
pedropompeu <pedrolpompeu@gmail.com>
2025-10-14 19:36:35 -0300

    feat(tasks): implement create task functionality

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
+import { CreateTaskDto } from 'src/auth/dto/create-task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
-
 export class TasksController {
-    @Get()
-    getTasksTest(): string {
-        return 'Rota de tarefa protegida';
-    }
+  constructor(private tasksService: TasksService) {}
+
+  @Post()
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ) {
+    return this.tasksService.createTask(createTaskDto, user);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { CreateTaskDto } from 'src/auth/dto/create-task.dto';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: Repository<Task>,
+  ) {}
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { titulo, descricao } = createTaskDto;
+
+    const task = this.tasksRepository.create({
+      titulo,
+      descricao,
+      user, 
+    });
+
+    await this.tasksRepository.save(task);
+    
+    return task;
+  }
+}
