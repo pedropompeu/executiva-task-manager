de9291543a01f752ab909f08000a13ecf640e340
pedropompeu <pedrolpompeu@gmail.com>
2025-10-14 20:57:25 -0300

    feat(frontend): setup initial react typescript project with create-react-app

--- src/tasks/dto/update-task-status.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { TaskStatus } from '../../tasks/task.entity';
-
-export class UpdateTaskStatusDto {
-  @IsEnum(TaskStatus, { message: `Status inválido. Os valores válidos são: ${Object.values(TaskStatus).join(', ')}` })
-  status: TaskStatus;
-}
