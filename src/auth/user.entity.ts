a1f2358c1db22ea7478b8519c411a16a1402ba03
pedropompeu <pedrolpompeu@gmail.com>
2025-10-14 16:46:44 -0300

    feat(auth): setup auth module and user entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, Unique} from 'typeorm';
+
+@Entity({name: 'users'})
+@Unique(['email'])
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({nullable: false,type: 'varchar', length: 200})
+    email: string;
+
+    @Column({nullable: false,type: 'varchar', length: 200})
+    password: string;
+}
