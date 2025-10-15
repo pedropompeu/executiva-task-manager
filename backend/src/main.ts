2d8fb1023c10f62bdbc4119d5a7294278fe0e16a
pedropompeu <pedrolpompeu@gmail.com>
2025-10-14 21:43:38 -0300

    fix(backend): enable CORS to allow frontend connections
    feat(frontend): implement user registration page

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,8 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors();
+  
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
